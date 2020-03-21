753ab2db50729e20b3ddf3b733458ed32400dd3a
gcyburt <36423263+gcyburt@users.noreply.github.com>
2020-03-21 20:36:08 +0100

    przciski się pojawiają, nie zmieniają ramek

--- GameMenu.py
+++ GameMenu.py
@@ -9,9 +9,7 @@ class GameMenu(tk.Frame):
         label = tk.Label(self, text="Menu główne")
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Nowy gracz",  # TODO: zrobić tak, żeby działało
-                            command=lambda: controller.show_frame("NewGame"))
-        button2 = tk.Button(self, text="Załaduj grę",
-                            command=lambda: controller.show_frame("LoadGame"))
+        button1 = tk.Button(text="Nowy gracz", command=lambda: controller.show_frame("NewGame")) #TODO: czemu nie przerzucaja ramek?
+        button2 = tk.Button(text="Załaduj grę", command=lambda: controller.show_frame("LoadGame"))
         button1.pack()
         button2.pack()
