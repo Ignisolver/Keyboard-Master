ad3170cba7f30c713cb14a042debbeb9f0d287ae
gcyburt <36423263+gcyburt@users.noreply.github.com>
2020-03-21 20:04:06 +0100

    dodane TODO

--- GameMenu.py
+++ GameMenu.py
@@ -9,7 +9,7 @@ class GameMenu(tk.Frame):
         label = tk.Label(self, text="Menu główne")
         label.pack(side="top", fill="x", pady=10)
 
-        button1 = tk.Button(self, text="Nowy gracz",
+        button1 = tk.Button(self, text="Nowy gracz",  # TODO: zrobić tak, żeby działało
                             command=lambda: controller.show_frame("NewGame"))
         button2 = tk.Button(self, text="Załaduj grę",
                             command=lambda: controller.show_frame("LoadGame"))
