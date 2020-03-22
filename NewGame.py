19cf6d2025e1486e4c375b2c4c72d19052428164
gcyburt <36423263+gcyburt@users.noreply.github.com>
2020-03-22 10:49:54 +0100

    jak w LoadGame

--- NewGame.py
+++ NewGame.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 
+LARGE_FONT = ("Verdana", 12)
+
 
 class NewGame(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        self.controller = controller
-        label = tk.Label(self, text="Menu główne")
-        label.pack(side="top", fill="x", pady=10)
+        label = tk.Label(self, text="Nowa gra", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+    # TODO: trzeba zrobić form na dane gracza i wybór poziomu trudności, no i pomyśleć nad ramką samej gry
