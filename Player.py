0b299b5b9eedb1a4c05a5a66b529066e2619990d
gcyburt <36423263+gcyburt@users.noreply.github.com>
2020-03-26 09:46:16 +0100

    utworzono klasę gracza

---
+++ Player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self, name, difficulty_level):
+        self.name = name
+        self.difficulty_level = difficulty_level
+
+    score_easy = 0
+    score_medium = 0
+    score_hard = 0
