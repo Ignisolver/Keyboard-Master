0ba7222c3dd7fb09fe0582e4cb9ca84590422aad
gcyburt <36423263+gcyburt@users.noreply.github.com>
2020-06-23 22:11:20 +0200

    Delete Player.py

--- Player.py
+++
@@ -1,7 +0,0 @@
-lass Player:
-    def __init__(self, name, difficulty_level):
-        self.name = name
-        self.difficulty_level = difficulty_level
-        self.score_easy = 0
-        self.score_medium = 0
-        self.score_hard = 0
