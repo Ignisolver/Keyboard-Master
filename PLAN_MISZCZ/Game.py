681956c4c3a2577607cbe9df1c5ea5477efb2b62
Ignacy Szkudelski <kaignacy@gmail.com>
2020-06-23 17:42:01 +0200

    dodanie funkcji input i kilku satłych

--- PLAN_MISZCZ/Game.py
+++ PLAN_MISZCZ/Game.py
@@ -1,50 +1,94 @@
+import sqlite3
 from threading import Thread
+import pygame
+
+database = r"..\db\mistrz_klawiatury.db"
+cx = sqlite3.connect(database)
+cu = cx.cursor()
 
 
-def pg_str_input():
-    """
-    #Ignacy
-    odczytuje wciśnity na klawiaturze klawisz
-    (funkcje należy umieśćić w pętli)
-    uwzgldnia litery polskie,cyfry,cpacje,CAPS,SHIFT,BACKSPACE,ENTER
-    funkcja do wklejenia w petle
-    Ignacy to robi (prawie gotowe)
-    :return: klawisz (klawisze) ktory został wciśnięty jako napis
-    """
-    global input_enable
-    input_enable = True
-    Thread(target=input_Thread).start()
+class Keyborder:
+    UPP = (12288, 4098, 4097, 28672, 20482, 20481, 12545, 12546, 8192, 2, 1, 24576, 16386, 16385, 8449, 8450)
+    alt = (20480, 28672, 20482, 20481, 12545, 12546, 16384, 24576, 16386, 16385, 8449, 8450)
+    code2letter = {97: 'a',
+                   98: 'b',
+                   99: 'c',
+                   100: 'd',
+                   101: 'e',
+                   102: 'f',
+                   103: 'g',
+                   104: 'h',
+                   105: 'i',
+                   106: 'j',
+                   107: 'k',
+                   108: 'l',
+                   109: 'm',
+                   110: 'n',
+                   111: 'o',
+                   112: 'p',
+                   113: 'q',
+                   114: 'r',
+                   115: 's',
+                   116: 't',
+                   117: 'u',
+                   118: 'w',
+                   119: 'v',
+                   120: 'x',
+                   121: 'y',
+                   122: 'z',
+                   32: ' '}
+    letter_alt = {
+        'l': 'ł',
+        'n': 'ń',
+        's': 'ś',
+        'o': 'ó',
+        'a': 'ą',
+        'e': 'ę',
+        'z': 'ż',
+        'x': 'ź',
+        'c': 'ć',
+    }
+    current_input = ''
+    finish = False
 
+    def pg_str_input(self):
+        """
+        #Ignacy
+        odczytuje wciskane na klawiaturze klawisze
+        jest uruchamiana w osobnym wątku
+        kończy działanie po naciśnięciu enter - ustawia atrbut finish na True
+        podczas działania zapisuje aktualny stan wpisywanego wyrazu do atrybutu current_input
+        """
+        self.finish = False
+        Thread(target=self.input_Thread).start()
 
-def input_Thread():
-    """
-    - zapisuje aktualny imput klawiatury do zmiennej globalnej
-    - wymaga ustawienia imput_enable na True na początku i na False na końcu
-    :return:
-    """
-    global current_input, pygame, code2letter, alt, letter_alt, UPP
-    while True:
-        letter = ''
-        if input_enable:
+    def input_Thread(self):
+        """
+        - zapisuje aktualny imput klawiatury do zmiennej globalnej
+        - wymaga ustawienia imput_enable na True na początku i na False na końcu
+        :return:
+        """
+        self.current_input = ''
+        while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit(0)
                 if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        self.finish = True
+                        return None
                     if event.key == pygame.K_BACKSPACE:
-                        if len(current_input) > 0:
-                            current_input = current_input[:-1]
-                    letter = code2letter.get(event.key, '')
-            mode = pygame.key.get_mods()
-            print(current_input)
-            if mode in alt:
-                letter = letter_alt.get(letter, letter)
-            if mode in UPP:
-                current_input += letter.upper()
-            else:
-                current_input += letter
-        else:
-            current_input = ''
+                        if len(self.current_input) > 0:
+                            self.current_input = self.current_input[:-1]
+                    letter = self.code2letter.get(event.key, '')
+                    mode = pygame.key.get_mods()
+                    if mode in self.alt:
+                        letter = self.letter_alt.get(letter, letter)
+                    if mode in self.UPP:
+                        self.current_input += letter.upper()
+                    else:
+                        self.current_input += letter
 
 
 def game_loop_chalange(level):
